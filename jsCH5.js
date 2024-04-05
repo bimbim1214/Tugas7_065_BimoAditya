df8a5212d81c39270f54ec7b5153f88fe5afee84
bimbim1214 <bimoadityapangestu@gmail.com>
2024-04-06 04:20:29 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video AUDI R8");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio AUDI R8");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar AUDI R8");
+});
